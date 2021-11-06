a5f9adc8930c078d2afa13476fd3c18509d7c14b
juanber <juanber@misena.edu.co>
2021-11-06 13:08:11 -0500

    Se usa para leer los archivos os con la ayuda de bufio

--- input_output.go
+++ input_output.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	var age int
-	var name string
-	fmt.Print("Enter your name: ")
-	fmt.Scanf("%s", &name)
-	fmt.Printf("Hola %s Ingresa tu edad: ", name)
-	fmt.Scanf("%d", &age)
-	fmt.Printf("Tu edad es: %d\n", age)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter text: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(text)
+	}
 }
